aldonunez/Loz_enhanced

commit 95941aee8858ea11c9664c09eec096407950a2dc

Make bitmap resources easier to see in the file system. Give them high-value green and blue channels.
Separate the underworld walls' tile layout from the image, so that the
layout can be used for other purposes.

Add a tool to analyze map layout compression.

Tools/ExtractLoz/Program.cs
@@ -474,18 +474,7 @@ namespace ExtractLoz
             int x = 0;
             int y = 0;
 
-            // Even though the graphics system can make a texture smaller than 16x16, 
-            // the texture is really 16x16 underneath. Use this size to calculate the 
-            // colors.
-
-            Color[] colors = new Color[] 
-            {
-                Color.FromArgb( 0, 0, 0 ),
-                Color.FromArgb( 16, 0, 0 ),
-                Color.FromArgb( 32, 0, 0 ),
-                Color.FromArgb( 48, 0, 0 ),
-            };
-
+            Color[] colors = GetPaletteContrastColors();
             byte[] tileIndexes = new byte[4];
 
             for ( int i = 0; i < 8; i++ )
@@ -527,41 +516,34 @@ namespace ExtractLoz
             var colors = GetPaletteStandInColors();
             int row = 0;
             int col = 0;
+            byte[,] map = new byte[22, 32];
 
             for ( int i = 0; i < 78; i++ )
             {
                 if ( wallTiles[i] != 0 )
                 {
-                    int tile = wallTiles[i];
+                    byte tile = wallTiles[i];
 
-                    SeekBgTile( reader, UWTileCHR, tile );
-                    DrawTile( reader, bmp, colors,
-                        col * 8 + 8, row * 8 + 8 );
+                    map[row + 1, col + 1] = tile;
 
                     tile = wallTiles[i];
                     if ( tile != 0xf5 && tile != 0xde )
                         tile++;
-                    SeekBgTile( reader, UWTileCHR, tile );
-                    DrawTile( reader, bmp, colors,
-                        (col) * 8 + 8, (10 - row) * 8 + 80 );
+                    map[(10 - row) + 10, col + 1] = tile;
 
                     tile = wallTiles[i];
                     if ( tile == 0xde || tile == 0xdc )
                         tile++;
                     else if ( tile != 0xf5 && tile != 0xe0 )
                         tile += 2;
-                    SeekBgTile( reader, UWTileCHR, tile );
-                    DrawTile( reader, bmp, colors,
-                        (12 - col) * 8 + 128 + 16, (10 - row) * 8 + 80 );
+                    map[(10 - row) + 10, (12 - col) + 16 + 2] = tile;
 
                     tile = wallTiles[i];
                     if ( tile == 0xde || tile == 0xe0 )
                         tile++;
                     else if ( tile != 0xf5 && tile != 0xdc )
                         tile += 3;
-                    SeekBgTile( reader, UWTileCHR, tile );
-                    DrawTile( reader, bmp, colors,
-                        (12 - col) * 8 + 128 + 16, (row) * 8 + 8 );
+                    map[row + 1, (12 - col) + 16 + 2] = tile;
                 }
 
                 row++;
@@ -574,22 +556,27 @@ namespace ExtractLoz
 
             for ( int i = 0; i < 32; i++ )
             {
-                SeekBgTile( reader, UWTileCHR, 0xF6 );
-                DrawTile( reader, bmp, colors,
-                    i * 8, 0 );
-                SeekBgTile( reader, UWTileCHR, 0xF6 );
-                DrawTile( reader, bmp, colors,
-                    i * 8, 168 );
+                map[0, i] = 0xF6;
+                map[21, i] = 0xF6;
             }
 
             for ( int i = 0; i < 20; i++ )
             {
-                SeekBgTile( reader, UWTileCHR, 0xF6 );
-                DrawTile( reader, bmp, colors,
-                    0, i * 8 + 8 );
-                SeekBgTile( reader, UWTileCHR, 0xF6 );
-                DrawTile( reader, bmp, colors,
-                    248, i * 8 + 8 );
+                map[i + 1, 0] = 0xF6;
+                map[i + 1, 31] = 0xF6;
+            }
+
+            for ( int r = 0; r < 22; r++ )
+            {
+                for ( int c = 0; c < 32; c++ )
+                {
+                    byte tile = map[r, c];
+                    if ( tile != 0 )
+                    {
+                        SeekBgTile( reader, UWTileCHR, tile );
+                        DrawTile( reader, bmp, colors, c * 8, r * 8 );
+                    }
+                }
             }
 
             using ( var g = Graphics.FromImage( bmp ) )
@@ -1092,7 +1079,7 @@ namespace ExtractLoz
         const int OWColDir = 0x19D0F + 16;
         const int OWColTables = 0x15BD8 + 16;
 
-        private static void ExtractOverworldMap( Options options )
+        private static MapLayout ExtractOverworldMap( Options options )
         {
             byte[] roomCols = null;
             ushort[] colTablePtrs = null;
@@ -1107,8 +1094,9 @@ namespace ExtractLoz
                 colTablePtrs = new ushort[16];
                 for ( int i = 0; i < 16; i++ )
                 {
-                    colTablePtrs[i] = reader.ReadUInt16();
+                    colTablePtrs[i] = (ushort) (reader.ReadUInt16() - colTablePtrs[0]);
                 }
+                colTablePtrs[0] = 0;
 
                 // There are only 10 columns in the last table
                 reader.BaseStream.Position = OWColTables;
@@ -1132,13 +1120,25 @@ namespace ExtractLoz
 
                 Utility.PadStream( writer.BaseStream );
             }
+
+            MapLayout mapLayout = new MapLayout();
+
+            mapLayout.uniqueRoomCount = 124;
+            mapLayout.columnsInRoom = 16;
+            mapLayout.rowsInRoom = 11;
+            mapLayout.owLayoutFormat = true;
+            mapLayout.roomCols = roomCols;
+            mapLayout.colTablePtrs = colTablePtrs;
+            mapLayout.colTables = colTables;
+
+            return mapLayout;
         }
 
         const int UWRoomCols = 0x160DE + 16;
         const int UWColDir = 0x16704 + 16;
         const int UWColTables = 0x162D6 + 16;
 
-        private static void ExtractUnderworldMap( Options options )
+        private static MapLayout ExtractUnderworldMap( Options options )
         {
             byte[] roomCols = null;
             ushort[] colTablePtrs = null;
@@ -1153,8 +1153,9 @@ namespace ExtractLoz
                 colTablePtrs = new ushort[10];
                 for ( int i = 0; i < 10; i++ )
                 {
-                    colTablePtrs[i] = reader.ReadUInt16();
+                    colTablePtrs[i] = (ushort) (reader.ReadUInt16() - colTablePtrs[0]);
                 }
+                colTablePtrs[0] = 0;
 
                 // There are only 9 columns in the last table
                 reader.BaseStream.Position = UWColTables;
@@ -1186,6 +1187,18 @@ namespace ExtractLoz
 
                 Utility.PadStream( writer.BaseStream );
             }
+
+            MapLayout mapLayout = new MapLayout();
+
+            mapLayout.uniqueRoomCount = 64;
+            mapLayout.columnsInRoom = 12;
+            mapLayout.rowsInRoom = 7;
+            mapLayout.owLayoutFormat = false;
+            mapLayout.roomCols = roomCols;
+            mapLayout.colTablePtrs = colTablePtrs;
+            mapLayout.colTables = colTables;
+
+            return mapLayout;
         }
 
         const int UWCellarRoomCols = 0x163B4 + 16;
@@ -1227,6 +1240,15 @@ namespace ExtractLoz
 
                 Utility.PadStream( writer.BaseStream );
             }
+        }
+
+        private static void AnalyzeUniqueLayouts( Options options )
+        {
+            var owLayout = ExtractOverworldMap( options );
+            var uwLayout = ExtractUnderworldMap( options );
+
+            Analyzer.AnalyzeUniqueLayouts( owLayout, "OW", options );
+            Analyzer.AnalyzeUniqueLayouts( uwLayout, "UW", options );
         }
 
         // Outer:
@@ -2636,9 +2658,9 @@ namespace ExtractLoz
             Color[] colors = new Color[] 
             {
                 Color.FromArgb( 0, 0, 0 ),
-                Color.FromArgb( 16, 0, 0 ),
-                Color.FromArgb( 32, 0, 0 ),
-                Color.FromArgb( 48, 0, 0 ),
+                Color.FromArgb( 16, 0x80, 0x00 ),
+                Color.FromArgb( 32, 0x00, 0x80 ),
+                Color.FromArgb( 48, 0x80, 0x80 ),
             };
             return colors;
         }

Tools/ExtractLoz/Analyzer.cs
@@ -1,0 +1,237 @@
+﻿/*
+   Copyright 2018 Aldo J. Nunez
+
+   Licensed under the Apache License, Version 2.0.
+   See the LICENSE text file for details.
+*/
+
+using System;
+using System.IO;
+
+namespace ExtractLoz
+{
+    struct MapLayout
+    {
+        public bool owLayoutFormat;
+        public int uniqueRoomCount;
+        public int columnsInRoom;
+        public int rowsInRoom;
+        public byte[] roomCols;
+        public ushort[] colTablePtrs;
+        public byte[] colTables;
+    }
+
+    class Analyzer
+    {
+        public static void AnalyzeUniqueLayouts( MapLayout layout, string typeName, Options options )
+        {
+            byte[,] map = new byte[layout.rowsInRoom, layout.columnsInRoom];
+            int mapSize = 1 * layout.rowsInRoom * layout.columnsInRoom;
+            int totalUncompressed = 0;
+            int totalCompressedRow = 0;
+            int totalCompressedFull = 0;
+
+            for ( int i = 0; i < layout.uniqueRoomCount; i++ )
+            {
+                if ( !ExtractUniqueLayout( layout, i, map ) )
+                    continue;
+
+                var compressedMapRleRow = CompressMapRleRow( map );
+                var compressedMapRleFull = CompressMapRleFull( map );
+
+                totalUncompressed += mapSize;
+                totalCompressedRow += compressedMapRleRow.Length;
+                totalCompressedFull += compressedMapRleFull.Length;
+
+                if ( options.AnalysisWrites )
+                {
+                    string filename = string.Format( "map.uncompressed.{0}.{1:D3}.bin", typeName, i );
+                    var filePath = options.MakeOutPath( filename );
+                    using ( var writer = new BinaryWriter( Utility.TruncateFile( filePath ) ) )
+                    {
+                        for ( int r = 0; r < map.GetLength( 0 ); r++ )
+                        {
+                            for ( int c = 0; c < map.GetLength( 1 ); c++ )
+                            {
+                                writer.Write( map[r, c] );
+                            }
+                        }
+                    }
+
+                    filename = string.Format( "map.compressed-rle-row.{0}.{1:D3}.bin", typeName, i );
+                    filePath = options.MakeOutPath( filename );
+                    File.WriteAllBytes( filePath, compressedMapRleRow );
+
+                    filename = string.Format( "map.compressed-rle-full.{0}.{1:D3}.bin", typeName, i );
+                    filePath = options.MakeOutPath( filename );
+                    File.WriteAllBytes( filePath, compressedMapRleFull );
+                }
+            }
+
+            float rowCompressionRatio = (totalCompressedRow / (float) totalUncompressed) * 100;
+            float fullCompressionRatio = (totalCompressedFull / (float) totalUncompressed) * 100;
+
+            Console.WriteLine( "map: {0} uncompressed size:      {1:N0}", typeName, totalUncompressed );
+            Console.WriteLine( "map: {0} compressed-by-row size: {1:N0} ({2:F0}%)", typeName, totalCompressedRow, rowCompressionRatio );
+            Console.WriteLine( "map: {0} compressed-full size:   {1:N0} ({2:F0}%)", typeName, totalCompressedFull, fullCompressionRatio );
+        }
+
+        private static bool ExtractUniqueLayout( MapLayout layout, int index, byte[,] map )
+        {
+            int MaxColumnStartOffset = (layout.columnsInRoom - 1) * layout.rowsInRoom;
+
+            int rowEnd = layout.rowsInRoom;
+            int roomCol = index * layout.columnsInRoom;
+
+            for ( int c = 0; c < layout.columnsInRoom; c++ )
+            {
+                byte columnDesc = layout.roomCols[roomCol];
+                int tableIndex = (columnDesc & 0xF0) >> 4;
+                int columnIndex = (columnDesc & 0x0F);
+
+                if ( tableIndex >= layout.colTablePtrs.Length )
+                    return false;
+
+                int k = 0;
+                int j = 0;
+                int colTableStart = layout.colTablePtrs[tableIndex];
+
+                for ( j = 0; j < MaxColumnStartOffset; j++ )
+                {
+                    byte t = layout.colTables[colTableStart + j];
+
+                    if ( (t & 0x80) != 0 )
+                    {
+                        if ( k == columnIndex )
+                            break;
+                        k++;
+                    }
+                }
+
+                for ( int r = 0; r < rowEnd; j++ )
+                {
+                    byte t = layout.colTables[colTableStart + j];
+                    int tileRef;
+
+                    if ( layout.owLayoutFormat )
+                        tileRef = t & 0x3F;
+                    else
+                        tileRef = t & 0x7;
+
+                    map[r++, c] = (byte) tileRef;
+
+                    if ( layout.owLayoutFormat )
+                    {
+                        if ( (t & 0x40) != 0 && r < rowEnd )
+                        {
+                            map[r++, c] = (byte) tileRef;
+                        }
+                    }
+                    else
+                    {
+                        int repeat = (t >> 4) & 0x7;
+                        for ( int m = 0; m < repeat && r < rowEnd; m++ )
+                        {
+                            map[r++, c] = (byte) tileRef;
+                        }
+                    }
+                }
+
+                roomCol++;
+            }
+
+            return true;
+        }
+
+        private static byte[] CompressMapRleRow( byte[,] map )
+        {
+            MemoryStream stream = new MemoryStream();
+            var compressor = new RleCompressor( stream );
+
+            for ( int r = 0; r < map.GetLength( 0 ); r++ )
+            {
+                for ( int c = 0; c < map.GetLength( 1 ); c++ )
+                {
+                    compressor.Push( map[r, c] );
+                }
+
+                compressor.Drain();
+            }
+
+            return stream.ToArray();
+        }
+
+        private static byte[] CompressMapRleFull( byte[,] map )
+        {
+            MemoryStream stream = new MemoryStream();
+            var compressor = new RleCompressor( stream );
+
+            for ( int r = 0; r < map.GetLength( 0 ); r++ )
+            {
+                for ( int c = 0; c < map.GetLength( 1 ); c++ )
+                {
+                    compressor.Push( map[r, c] );
+                }
+            }
+
+            compressor.Drain();
+
+            return stream.ToArray();
+        }
+
+        class RleCompressor
+        {
+            MemoryStream stream;
+            int count;
+            byte value;
+
+            public RleCompressor( MemoryStream stream )
+            {
+                this.stream = stream;
+            }
+
+            public void Push( byte value )
+            {
+                if ( count == 0 )
+                {
+                    count = 1;
+                    this.value = value;
+                }
+                else if ( value == this.value )
+                {
+                    count++;
+                    if ( count == 256 )
+                        DrainInternal();
+                }
+                else
+                {
+                    DrainInternal();
+                    count = 1;
+                    this.value = value;
+                }
+            }
+
+            private void DrainInternal()
+            {
+                if ( count == 1 )
+                {
+                    stream.WriteByte( value );
+                }
+                else
+                {
+                    int element = (count - 1) | 0x80;
+                    stream.WriteByte( (byte) element );
+                    stream.WriteByte( value );
+                }
+
+                count = 0;
+            }
+
+            public void Drain()
+            {
+                if ( count != 0 )
+                    DrainInternal();
+            }
+        }
+    }
+}

Tools/ExtractLoz/ExtractLoz.csproj
@@ -48,6 +48,7 @@
     <Reference Include="System.Xml" />
   </ItemGroup>
   <ItemGroup>
+    <Compile Include="Analyzer.cs" />
     <Compile Include="DatafileReader.cs" />
     <Compile Include="DefaultSystemPalette.cs" />
     <Compile Include="ExtensionMethods.cs" />

Tools/ExtractLoz/Options.cs
@@ -19,6 +19,7 @@ namespace ExtractLoz
         public string Function;
         public string OutPath;
         public string Error;
+        public bool AnalysisWrites;
 
         public static Options Parse( string[] args )
         {
@@ -64,6 +65,10 @@ namespace ExtractLoz
                         break;
                     }
                     i++;
+                }
+                else if ( args[i].EqualsIgnore( "-analysis-writes" ) )
+                {
+                    options.AnalysisWrites = true;
                 }
             }
 

commit aaf9b332222087a5f633ef28718cbe8f831bbdee

Fix the colors of the underworld tiles.
This fixes a problem introduced in 95941ae that changed stand-in colors to contrast colors.

Tools/ExtractLoz/Program.cs
@@ -474,7 +474,7 @@ namespace ExtractLoz
             int x = 0;
             int y = 0;
 
-            Color[] colors = GetPaletteContrastColors();
+            Color[] colors = GetPaletteStandInColors();
             byte[] tileIndexes = new byte[4];
 
             for ( int i = 0; i < 8; i++ )